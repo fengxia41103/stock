a9d3aa27d12ea94fa794f58cf16de6bb37705f4d
feng xia <feng_xia41103@hotmail.com>
2021-02-27 15:37:46 -0500

    Add terminal value to DCF.

--- frontend/src/stocks/dcf.jsx
+++ frontend/src/stocks/dcf.jsx
@@ -18,11 +18,19 @@ class DCF extends Component {
       capital_structure = Math.floor(tmp[0]);
     }
 
+    // get free cash flow from cach flow statements
+    tmp = map(stock.cashes, b => b.free_cash_flow);
+    tmp = filter(reverse(tmp), x => x > 0);
+    const free_cash_flow = tmp[0];
+
     this.state = {
       risk_free: 1.242,
       market_premium: 7,
       cost_of_debt: 10,
       growth_rate: 7,
+      project_year: 10,
+      fcf: free_cash_flow,
+      terminal_growth_rate: 5,
       capital_structure: capital_structure,
     };
 
@@ -30,6 +38,8 @@ class DCF extends Component {
     this.cost_of_debt_change = this.cost_of_debt_change.bind(this);
     this.growth_rate_change = this.growth_rate_change.bind(this);
     this.capital_structure_change = this.capital_structure_change.bind(this);
+    this.project_year = this.project_year.bind(this);
+    this.terminal_growth = this.terminal_growth.bind(this);
   }
 
   market_premium_change(event) {
@@ -52,6 +62,16 @@ class DCF extends Component {
       capital_structure: event.target.value,
     });
   }
+  project_year(event) {
+    this.setState({
+      project_year: event.target.value,
+    });
+  }
+  terminal_growth(event) {
+    this.setState({
+      terminal_growth_rate: event.target.value,
+    });
+  }
 
   render() {
     const { stock } = this.props;
@@ -61,6 +81,9 @@ class DCF extends Component {
       cost_of_debt,
       growth_rate,
       capital_structure,
+      project_year,
+      terminal_growth_rate,
+      fcf,
     } = this.state;
 
     // if this is a ETF, skip
@@ -91,7 +114,7 @@ class DCF extends Component {
     let income = 0;
 
     // year 0-5, growing at growth rate
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= Math.floor(project_year / 2); i++) {
       let tmp = cash_flow * Math.pow(1 + growth_rate / 100, i);
       let discounted = tmp / Math.pow(1 + wacc, i);
       income += discounted;
@@ -104,14 +127,23 @@ class DCF extends Component {
     // - the cost of debt, eg. rating of this company, is not changing
     //
     // Well, these are a lot to assume!
-    for (let i = 6; i <= 10; i++) {
+    for (let i = Math.floor(project_year / 2) + 1; i <= project_year; i++) {
       let tmp = cash_flow * Math.pow(1 + growth_rate / 200, i);
       let discounted = tmp / Math.pow(1 + wacc, i);
       income += discounted;
     }
 
+    // terminal value
+    // (FCF * (1 + g)) / (d - g)
+    let terminal_value = 0;
+    if (wacc > terminal_growth_rate / 100) {
+      let terminal = terminal_growth_rate / 100;
+
+      terminal_value = (fcf * (1 + terminal)) / (wacc - terminal);
+    }
+
     // Note: we are assuming no. of stocks dont' change in 10 years!
-    let dcf = income / stock.shares_outstanding;
+    let dcf = (income + terminal_value) / stock.shares_outstanding;
 
     return (
       <div className="jumbotron">
@@ -134,11 +166,26 @@ class DCF extends Component {
             <div className="quotation">{cash_flow.toFixed(2)}</div>
           </div>
           <div className="col l4 m6 s12 card">
-            <h4 className="mylabel">10-yr Projected CF</h4>
+            <h4 className="mylabel">{project_year}-yr Projected CF</h4>
             <div className="quotation">{income.toFixed(2)}</div>
+          </div>
+          <div className="col l4 m6 s12 card">
+            <h4 className="mylabel">Terminal Value</h4>
+            <div className="quotation">{terminal_value.toFixed(2)}</div>
           </div>
         </div>
         <div className="row">
+          <div className="col l3 m6 s12">
+            <h4 className="mylabel">Project Years</h4>
+            <DebounceInput
+              className="input-field"
+              debounceTimeout={1000}
+              value={project_year}
+              type="number"
+              min={2}
+              onChange={this.project_year}
+            />
+          </div>
           <div className="col l3 m6 s12">
             <h4 className="mylabel">Growth Rate %</h4>
             <DebounceInput
@@ -147,6 +194,17 @@ class DCF extends Component {
               value={growth_rate}
               type="number"
               onChange={this.growth_rate_change}
+            />
+          </div>
+          <div className="col l3 m6 s12">
+            <h4 className="mylabel">Ternimal Rate %</h4>
+            <DebounceInput
+              className="input-field"
+              debounceTimeout={1000}
+              value={terminal_growth_rate}
+              type="number"
+              max={(wacc * 100).toFixed(2)}
+              onChange={this.terminal_growth}
             />
           </div>
           <div className="col l3 m6 s12">
