71426b1d2dd6b8fc5118a50b03ca74e0e3b3ab63
feng xia <feng_xia41103@hotmail.com>
2021-08-10 13:07:14 -0400

    feat: Commitlint scope case be UPPERCASE
    Update commitlint rule to enforce scope using UPPERCASE.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "scope-case": [2, "always", "upper-case"],
     "subject-case": [2, "always", "sentence-case"],
     "subject-max-length": [2, "always", 50],
   },
