eb8fdd4374f668da9ca68154946ba570e29e9002
feng xia <feng_xia41103@hotmail.com>
2021-08-11 17:31:32 -0400

    style: Adjust symbol width in stock list for xs

--- frontend/src/components/stock/ListStockCard/index.jsx
+++ frontend/src/components/stock/ListStockCard/index.jsx
@@ -67,10 +67,10 @@ export default function ListStockCard(props) {
     return (
       <ListItem key={s.id} divider={true}>
         <Grid container spacing={1} alignItems="center">
-          <Grid item lg={2} xs={2}>
+          <Grid item lg={2} xs={3}>
             <StockSymbol {...s} />
           </Grid>
-          <Grid item lg={4} xs={4}>
+          <Grid item lg={4} xs={3}>
             <RecentPriceSparkline stock={s.id} />
           </Grid>
           <Grid item lg={2} xs={2}>
