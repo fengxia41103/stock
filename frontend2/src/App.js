043dadfc2ff18bfc92e73816839c66f276c66f3c
Feng Xia <feng.xia@lucidum.io>
2023-01-13 13:11:22 -0500

    feat: Migrate frontend to new CRA

---
+++ frontend2/src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+
+import React from "react";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import { useLocation, useRoutes } from "react-router-dom";
+import { RestfulProvider } from "restful-react";
+
+import { ThemeProvider } from "@mui/material";
+
+import GlobalContext from "./context";
+// import routes from "src/routes";
+import theme from "./theme";
+import LoginView from "./views/auth/LoginView";
+import RegistrationView from "./views/auth/RegistrationView";
+
+const globals = {
+  backend: {
+    api: "http://localhost:8003/api/v1",
+    host: "http://localhost:8003",
+  },
+};
+
+const App = () => {
+  // global config
+  const { backend } = globals;
+
+  // check authentication
+  const session = window.sessionStorage;
+  const user = session.getItem("user");
+  const api_key = session.getItem("api_key");
+
+  // routing table
+  // const routing = useRoutes(routes);
+  const location = useLocation();
+
+  // goto where
+  let here = "hello world";
+  if (location.pathname === "/registration") {
+    here = <RegistrationView />;
+  } else if (!!user && !!api_key) {
+    // here = routing;
+    here = "lsjkdfldjsfldjs";
+  } else {
+    here = <LoginView />;
+  }
+
+  const auth = `ApiKey ${user}:${api_key}`;
+  return (
+    <ThemeProvider theme={theme}>
+      <RestfulProvider
+        base={backend.api}
+        requestOptions={() => ({
+          headers: {
+            "content-type": "application/json",
+            Authorization: auth,
+          },
+        })}
+      >
+        <GlobalContext.Provider value={{ ...backend, user, auth }}>
+          {here}
+        </GlobalContext.Provider>
+      </RestfulProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ frontend2/src/views/auth/LoginView/index.jsx
@@ -0,0 +1,72 @@
+import clsx from "clsx";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Box, Grid, Paper } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import LoginCard from "@Components/auth/LoginCard";
+import GlobalContext from "@/context";
+
+const myStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: "30vh",
+  },
+  paper: {
+    padding: theme.spacing(3),
+    backgroundImage: `url(${"/static/images/auth.jpeg"})`,
+    height: "100vh",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    backgroundSize: "cover",
+    backgroundAttachment: "fixed",
+  },
+}));
+
+const LoginView = () => {
+  const { api } = useContext(GlobalContext);
+  const classes = myStyles();
+  const navigate = useNavigate();
+
+  // states
+  const [resource] = useState("/auth/login/");
+  const [error, setError] = useState();
+
+  // callbacks
+  const session = window.sessionStorage;
+  const save_auth = (resp) => {
+    session.setItem("user", resp.data.user);
+    session.setItem("api_key", resp.data.key);
+  };
+
+  // you decide what to do if logged in
+  const on_success = (resp) => {
+    if (resp.success) {
+      // save auth somewhere for all calls
+      save_auth(resp);
+
+      // go to a landing page
+      navigate("/", true);
+    } else {
+      setError(resp.message);
+    }
+  };
+
+  // render
+  return (
+    <Paper className={clsx(classes.paper)}>
+      <Grid container justifyContent="center" className={classes.margin}>
+        <Grid item lg={6} sm={6} xs={12} />
+        <Grid item lg={4} sm={5} xs={12}>
+          <Box className={clsx(classes.card)}>
+            <LoginCard
+              {...{ resource: `${api}${resource}`, on_success, error }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
+
+export default LoginView;
