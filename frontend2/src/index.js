70e95717f46675b1d80072d1375012b17998280d
Feng Xia <feng.xia@lucidum.io>
2023-01-22 15:37:31 -0500

    fix: Fix resolver path

--- frontend2/src/index.js
+++ frontend2/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+import { ThemeProvider } from "@mui/material";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
