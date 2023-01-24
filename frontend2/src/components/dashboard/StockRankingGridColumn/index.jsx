44fcd8df895aa818ee081e3f3aed2973c229beac
Feng Xia <feng.xia@lucidum.io>
2023-01-24 09:08:20 -0500

    feat: Trending random color is working

--- frontend2/src/components/dashboard/StockRankingGridColumn/index.jsx
+++ frontend2/src/components/dashboard/StockRankingGridColumn/index.jsx
@@ -1,39 +1,36 @@
-import clsx from "clsx";
 import { map } from "lodash";
 import PropTypes from "prop-types";
 import React from "react";
 
 import { Grid, Link, Typography } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { styled } from "@mui/material/styles";
 
 import { HighlightedText } from "@fengxia41103/storybook";
-
-const myStyles = makeStyles(() => ({
-  category: {
-    color: "#D52349",
-  },
-}));
 
 const StockRankingGridColumn = (props) => {
   const { category, stocks, order_by, highlights } = props;
 
-  const classes = myStyles();
+  const ranks = map(stocks, (stock) => {
+    const val = stock[order_by];
+    const { symbol, stock_id } = stock;
+    const mycolor = highlights[symbol];
 
-  const ranks = map(stocks, (p) => {
-    const val = p[order_by];
     return (
-      <Grid item key={p.stock_id} xs>
-        <Link href={`/stocks/${p.stock_id}/historical/price`}>
-          <HighlightedText {...{ highlights, text: p.symbol, val }} />
+      <Grid item key={stock_id} xs>
+        <Link href={`/stocks/${stock_id}/historical/price`}>
+          <HighlightedText {...{ highlights: mycolor, text: symbol, val }} />
         </Link>
       </Grid>
     );
   });
 
+  const MyColumnHeader = styled(Typography)({
+    color: "#d52349",
+  });
   return (
     <Grid container direction="column" alignContent="center">
       <Grid item xs>
-        <Typography className={clsx(classes.category)}>{category}</Typography>
+        <MyColumnHeader>{category}</MyColumnHeader>
       </Grid>
       {ranks}
     </Grid>
@@ -49,7 +46,7 @@ StockRankingGridColumn.propTypes = {
       symbol: PropTypes.string,
     }),
   ).isRequired,
-  highlights: PropTypes.node.isRequired,
+  highlights: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
 export default StockRankingGridColumn;
