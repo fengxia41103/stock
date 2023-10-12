13de45d3bd3ff21b5734b5fb81d5f63c02bdd06a
Feng Xia <feng_xia41103@hotmail.com>
2023-10-12 13:23:17 -0400

    fix: Remove unused dashboard cards

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,3 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
 import AnnouncementIcon from "@mui/icons-material/Announcement";
 import BusinessIcon from "@mui/icons-material/Business";
 import CompareIcon from "@mui/icons-material/Compare";
@@ -55,6 +52,9 @@ import StockSummaryView from "@Views/stock/StockSummaryView";
 import TechIndicatorView from "@Views/stock/TechIndicatorView";
 import TwentyFourHourReturnView from "@Views/stock/TwentyFourHourReturnView";
 import ValuationRatiosView from "@Views/stock/ValuationRatiosView";
+
+import React from "react";
+import { Navigate } from "react-router-dom";
 
 const navbar_items = [
   {
