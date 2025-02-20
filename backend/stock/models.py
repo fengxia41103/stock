5dd8139280cf7ff77708d9d6a2d90c28fe26dad3
Feng Xia <feng_xia41103@hotmail.com>
2025-02-19 21:11:46 -0500

    feat: Use yfinance to pull historical price

--- backend/stock/models.py
+++ backend/stock/models.py
@@ -360,7 +360,7 @@ class MyStock(models.Model):
         tmp = self.balances.order_by("-on").first()
         if tmp:
             cash_per_share = tmp.cash_and_cash_equivalent_per_share
-            if cash_per_share:
+            if self.latest_close_price and cash_per_share:
                 return self.latest_close_price / cash_per_share
             else:
                 return None
